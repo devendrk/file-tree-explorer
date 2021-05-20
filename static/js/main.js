4e7e52ba98695c2aacd43fc0c3093cd47e7091e2
Devendra Katwal <katwaldevendra1@gmail.com>
2021-05-20 11:29:46 +0300

    start2 #

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,19 @@
 import { fetchApi } from "./apiCall.js";
 fetchApi();
 
-var hierarchy = document.getElementById("hierarchy");
-hierarchy.addEventListener("click", function (event) {
-  var elem = event.target;
-  if (elem.tagName.toLowerCase() === "li" && elem !== event.currentTarget) {
-    var type = elem.classList.contains("far") ? "folder" : "file";
+var toggler = document.getElementsByClassName("caret");
+var i;
 
-    if (type == "folder") {
-      var isexpanded = elem.dataset.isexpanded == "true";
-      if (isexpanded) {
-        elem.classList.remove("fa-caret-down");
-        elem.classList.add("fa-caret-right");
-      } else {
-        elem.classList.remove("fa-caret-right");
-        elem.classList.add("fa-caret-down");
-      }
-      elem.dataset.isexpanded = !isexpanded;
-
-      var toggleelems = [].slice.call(elem.parentElement.children);
-      var classnames = "file,foldercontainer,noitems".split(",");
-
-      toggleelems.forEach(function (element) {
-        if (
-          classnames.some(function (val) {
-            return element.classList.contains(val);
-          })
-        )
-          element.style.display = isexpanded ? "none" : "block";
-      });
-    }
-  }
-});
+for (i = 0; i < toggler.length; i++) {
+  console.log("i", i, "toggler", toggler[i]);
+  toggler[i].addEventListener("click", function () {
+    console.log(
+      "parent element",
+      this.parentElement.querySelector(".nested"),
+      "thiiiss",
+      this
+    );
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
