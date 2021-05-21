37bf8831a1e50a8eba8e0fa86a3dd3db6808df6f
Devendra Katwal <katwaldevendra1@gmail.com>
2021-05-21 11:11:43 +0300

    create element

--- static/js/main.js
+++ static/js/main.js
@@ -9,46 +9,65 @@ fetch("../filesystem.json")
 
     // Examine the text in the response
     response.json().then(function (data) {
-      document.querySelector("#root").innerText = data.path;
-      console.log("data...", data);
-      createNode(data);
+      const filterChildren = data.children.filter((d, index) => index < 4);
+      const newData = { ...data, children: filterChildren };
+      console.log("new data", newData);
+      // createNode(newData);
+      recursion(newData);
     });
   })
   .catch(function (err) {
     console.log("Fetch Error :-S", err);
   });
 
-function generateDirectoryList(data) {
-  console.log("i am from fu", data, "l..", data.children.length);
-  var toggler = document.getElementsByClassName("folder");
-  console.log(toggler.length);
-  for (let i = 0; i < toggler.length; i++) {
-    console.log("i", i, "toggler", toggler[i]);
-    toggler[i].addEventListener("click", function () {
-      this.parentElement.querySelector(".file").classList.toggle("expanded");
-      this.classList.toggle("folder-down");
-    });
-  }
-}
+// function generateDirectoryList() {
+//   var toggler = document.getElementsByClassName("folder");
+//   console.log(toggler.length);
+//   for (let i = 0; i < toggler.length; i++) {
+//     console.log("i", i, "toggler", toggler[i]);
+//     toggler[i].addEventListener("click", function () {
+//       this.parentElement.querySelector(".file").classList.toggle("expanded");
+//       this.classList.toggle("folder-down");
+//     });
+//   }
+// }
 
-function createElement(el, attributeName) {
-  const element = document.createElement(el, { class: attributeName });
-  console.log("element created", element);
+/**
+ *
+ * @param {string} tag
+ * @param {string} attributeName
+ * @returns HTML element such as <P class = "attribute name"> </p>
+ */
+function createTag(tag, attribute) {
+  let element = document.createElement(tag);
+  element.setAttribute("class", attribute);
   return element;
 }
+let li = createTag("li", "folder");
+li.textContent = "fuck";
+console.log(li);
 
 // recursive array
-function createNode(data) {
+const parentEl = document.getElementById("#fileBrowser1");
+function RenderFileTree(data) {
   if (data === null || data === undefined) return;
   if (data.children !== undefined) {
-    console.log("folder", data.name, data.path);
+    // console.log("folder", data.name, data.path);
+    let li = createTag("li", "folder");
+    li.innerText = data.name;
+    console.log("FOLDER: liii...", li, "dnamee:", data.name);
     data.children.forEach((fs) => {
-      createNode(fs);
+      RenderFileTree(fs);
+      let fileHolderUl = createTag("ul", "files");
+      return fileHolderUl;
     });
   } else {
-    console.log("file", data.path);
+    // console.log("file", data.path);
+    let fileHolderLi = createTag("li", "single-file");
+    fileHolderLi.textContent = data.name;
+    fileHolderUl.appendChild(fileHolderLi);
+    console.log("file Ul", fileHolderUl);
   }
-
   // for (let i = 0; i < arr.length; i++) {
   //   if (arr[i] === null) {
   //     console.log("empty file or folder", arr[i]);
